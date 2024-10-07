17bcab028a656650d62965743d4266eb554297e9
samWidlund <sam.wid415@gmail.com>
2024-10-07 22:45:52 +0200

    add fadein and styling for navbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,15 @@
+// fadein effect for garderobenwLogo
 document.addEventListener("DOMContentLoaded", function() {
-    const myDiv = document.getElementById("garderobenwLogo");
-    myDiv.classList.add("show");
+    const garderobenwLogo = document.getElementById("garderobenwLogo");
+    garderobenwLogo.classList.add("show");
+});
+
+window.addEventListener("scroll", function() {
+    const navbar = document.getElementById("navbar");
+    // Check if the page is scrolled down
+    if (window.scrollY > 50) { // Adjust the value as needed
+        navbar.classList.add("show");
+    } else {
+        navbar.classList.remove("show");
+    }
 });
