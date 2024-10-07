0050a359af9356e09237da2f2c8aeaa8cc139367
samWidlund <sam.wid415@gmail.com>
2024-10-07 13:15:38 +0200

    add new styling

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+const garderobenLogo = document.getElementById('garderobenwLogo');
+
+window.addEventListener('load', () => {
+    garderobenLogo.classList.add('fade-in'); // Add the fade-in class on load
+});
