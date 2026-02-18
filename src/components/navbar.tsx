d0ee47de55e35efc1e1e41b898d407a01630c971
samWidlund <sam.wid415@gmail.com>
2026-02-18 13:17:49 +0100

    navbar styling

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,10 +5,15 @@ export default function Navbar() {
   return (
     <nav className="navbar">
     <img className='logo' src={logo} alt="garderobenwlogo-nobg.png" />
+      <div className="shop">
+        <a href="/mens">mens</a>
+        <a href="/womens">womens</a>
+        <a href="/shop">shop_all</a>        
+      </div>
       <div className="nav-links">
-        <a href="/home">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
+        <a href="/home">home</a>
+        <a href="/about">about</a>
+        <a href="/contact">contact</a>
       </div>
     </nav>
   )
