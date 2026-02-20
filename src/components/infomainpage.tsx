4d8858867955602a61a0e8ff216f666ca7bde26a
samWidlund <sam.wid415@gmail.com>
2026-02-20 17:47:19 +0100

    add * to prada and fix css indentation

--- src/components/infomainpage.tsx
+++ src/components/infomainpage.tsx
@@ -6,7 +6,7 @@ export default function InfoMainPage() {
         <h1>*our legacy</h1>
         <h1>*arc'teryx</h1>
         <h1>*haglöfs</h1>
-        <h1>prada_</h1>
+        <h1>*prada</h1>
         <h1>*vintage</h1>
         <h1>*acne studios</h1>
     </div>
