029f34d5f019fc96bc9671d44509dfe442533f17
samWidlund <sam.wid415@gmail.com>
2026-02-20 18:06:47 +0100

    add aboutpage and fix border

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import LandingPage from './components/landingpage'
 import NavBar from './components/navbar'
 import MainPage from './components/mainpage'
 import Footer from './components/footer'
+import AboutPage from './components/aboutpage'
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <main>
         <LandingPage />
         <MainPage />
+        <AboutPage />
       </main>
       <Footer />
     </>
---
+++ src/components/aboutpage.tsx
@@ -0,0 +1,9 @@
+import '../App.css'
+
+
+export default function MainPage() {
+  return (
+    <div className='main-page'>
+    </div>
+  )
+}
