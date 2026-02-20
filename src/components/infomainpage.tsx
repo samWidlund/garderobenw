6a9c0156399fcfa4263d4d9413e1cafcfe4c168c
samWidlund <sam.wid415@gmail.com>
2026-02-20 17:41:55 +0100

    infomainpage in mainpage

---
+++ src/components/infomainpage.tsx
@@ -0,0 +1,14 @@
+import '../App.css'
+
+export default function InfoMainPage() {
+  return (
+    <div className='info-main-page'>
+        <h1>*our legacy</h1>
+        <h1>*arc'teryx</h1>
+        <h1>*haglöfs</h1>
+        <h1>prada_</h1>
+        <h1>*vintage</h1>
+        <h1>*acne studios</h1>
+    </div>
+  )
+}
--- src/components/mainpage.tsx
+++ src/components/mainpage.tsx
@@ -1,10 +1,12 @@
 import '../App.css'
 import VideoMainPage from './videomainpage'
+import InfoMainPage from './infomainpage'
 
 export default function MainPage() {
   return (
     <div className='main-page'>
       <VideoMainPage />
+      <InfoMainPage />
     </div>
   )
 }
