16c9ed275bfa99d68dfd5d821942fe0f450d9b6f
samWidlund <sam.wid415@gmail.com>
2026-02-20 18:15:20 +0100

    garderoben styling effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import LandingPage from './components/landingpage'
 import NavBar from './components/navbar'
 import MainPage from './components/mainpage'
@@ -6,10 +7,21 @@ import AboutPage from './components/aboutpage'
 import './App.css'
 
 function App() {
+  const [scrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > window.innerHeight)
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (
     <>
       <NavBar />
       <main>
+        <p id="garderoben" className={scrolled ? 'absolute' : ''}>garderoben</p>
         <LandingPage />
         <MainPage />
         <AboutPage />
@@ -18,5 +30,4 @@ function App() {
     </>
   )
 }
-
 export default App
--- src/components/landingpage.tsx
+++ src/components/landingpage.tsx
@@ -3,7 +3,6 @@ import '../App.css'
 export default function LandingPage() {
   return (
     <div id="landing-page">
-      <p id="garderoben">garderoben</p>
       <p id="W">W</p>
     </div>
   );
