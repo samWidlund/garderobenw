b55931fc06af41e62f28696562745cc4bb50873e
samWidlund <sam.wid415@gmail.com>
2026-02-20 17:46:32 +0100

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import LandingPage from './components/landingpage'
 import NavBar from './components/navbar'
 import MainPage from './components/mainpage'
+import Footer from './components/footer'
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <LandingPage />
         <MainPage />
       </main>
+      <Footer />
     </>
   )
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+import '../App.css'
+
+export default function MainPage() {
+  return (
+    <div className="footer">
+        @2026 garderoben.w
+    </div>
+)
+}
